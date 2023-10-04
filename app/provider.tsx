34059dbd354554e2e520dc339b7cfe4e69f748de
Afsal <afsalpu07@gmail.com>
2023-10-04 20:37:57 +0530

    responsive design partially implemented

--- app/provider.tsx
+++ app/provider.tsx
@@ -5,7 +5,7 @@
 import { ThemeProvider as TP } from "next-themes"
 
 export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
-    return <TP attribute="class">{children}</TP>
+    return <TP attribute="class" >{children}</TP>
 }
 
 
